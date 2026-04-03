f9ee7992f8ad19b8d57beab3042c9a20ce79495f
live <live0x@outlook.com>
2026-04-03 22:02:46 +0800

    create

--- src/main.tsx
+++ src/main.tsx
@@ -1,38 +1,10 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.tsx";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
 
-import {
-  PublicClientApplication,
-  EventType,
-  EventMessage,
-  AuthenticationResult,
-} from "@azure/msal-browser";
-import { msalConfig } from "./authConfig";
-
-export const msalInstance = new PublicClientApplication(msalConfig);
-await msalInstance
-  .initialize()
-  .then(() => {
-    const accounts = msalInstance.getAllAccounts();
-    if (accounts.length > 0) {
-      msalInstance.setActiveAccount(accounts[0]);
-    }
-
-    msalInstance.addEventCallback((event: EventMessage) => {
-      if (event.eventType === EventType.LOGIN_SUCCESS && event.payload) {
-        const payload = event.payload as AuthenticationResult;
-        msalInstance.setActiveAccount(payload.account);
-      }
-    });
-
-    createRoot(document.getElementById("root")!).render(
-      <StrictMode>
-        <App pca={msalInstance} />
-      </StrictMode>
-    );
-  })
-  .catch((error) => {
-    console.error("MSAL Init:", error);
-  });
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
